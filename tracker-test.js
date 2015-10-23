7916ee830e5d8562dbd7b024b5ececc5f7b70106
Igor Loskutov <igor.loskutoff@gmail.com>
2015-10-23 14:13:54 +0300

    init

---
+++ tracker-test.js
@@ -0,0 +1,30 @@
+if (Meteor.isClient) {
+
+  var HelloMessage = React.createClass({displayName: "HelloMessage",
+    render: function() {
+      notExist();
+      return React.createElement("div", null, "Hello ", this.props.name);
+    }
+  });
+
+  var templateName = 'hello';
+
+  var templateClass = new Template(
+    templateName,
+    function() {
+      // A placeholder HTML element that will serve as the mounting
+      // point for the React component.
+      return new HTML.DIV;
+    }
+  );
+
+  Template[templateName] = templateClass;
+
+  templateClass.onRendered(function() {
+    React.render(React.createElement(HelloMessage, {name: "John"}), this.firstNode);
+  });
+}
+
+if (Meteor.isServer) {
+  Kadira.connect('oYLWc4o9kfyCdb69s', '46d8d21e-1b2b-44ba-b08d-1e225f88b4a6');
+}
